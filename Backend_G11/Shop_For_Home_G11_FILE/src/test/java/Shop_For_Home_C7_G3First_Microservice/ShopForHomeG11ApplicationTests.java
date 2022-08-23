2f1f18e542dd23c43ad83276dec5db9930413d25
KARRA SAI KARTHIK <82261397+Karthik-2598@users.noreply.github.com>
2022-08-23 16:47:21 +0530

    Update ShopForHomeG11

--- Backend_G11/Shop_For_Home_G11_FILE/src/test/java/Shop_For_Home_C7_G3First_Microservice/ShopForHomeG11ApplicationTests.java
+++ Backend_G11/Shop_For_Home_G11_FILE/src/test/java/Shop_For_Home_C7_G3First_Microservice/ShopForHomeG11ApplicationTests.java
@@ -1,10 +1,10 @@
-package Shop_For_Home_C7_G3First_Microservice;
+package Shop_For_Home_G11_FILE;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShopForHomeC7G3FirstMicroserviceApplicationTests {
+class ShopForHomeG11ApplicationTests {
 
 	@Test
 	void contextLoads() {
